55e536c60fe579001bf20057db9a039d2db65d1e
nchuv <natalya_chuvi@mail.ru>
2015-08-16 15:40:43 +0300

    menu xs

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,11 @@
 
 
 $(document).ready(function(){
+
+	$('.navigation__trigger').on("click", function(){
+		$('.navigation__list').slideToggle();
+	})
+
 	$('.bxslider').bxSlider({
 		// auto: true,
 		pager: true
