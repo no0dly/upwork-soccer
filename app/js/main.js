26be83e59321f1c9e2e4a406d83142b29e372805
Vadim Derman <thenoodly@gmail.com>
2015-08-16 14:49:27 -0700

    waypoints

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,64 @@
-// var app = (function () {
+var app = (function () {
 
-// 	$('.bxslider').bxSlider({
+	function setUpListeners(){
+		$('.navigation__trigger').on("click", _triggerNav);
+		$('.navigation__item a[href^="#"]').on('click', _scrolling);
+	}
 
-// 	});
+	function _triggerNav() {
+		
+		var
+			menu = $('.navigation__list');
 
-// }());
+		menu.slideToggle();
+	}
+
+	function _scrolling(e) {
+		e.preventDefault();
+		var
+			elementClick = $(this).attr("href"),
+			destination = $(elementClick).offset().top,
+			body = $('body');
 
 
-$(document).ready(function(){
+		body.animate( { scrollTop: destination }, 1100 );
+	}
 
-	$('.navigation__trigger').on("click", function(){
-		$('.navigation__list').slideToggle();
-	});
+	function _waypoint () {
+		
+		var
+			$this = $(this.element),
+			navLi = $('.navigation__item'),
+			hash = '#' + $this.attr('id');
 
-	$('.bxslider').bxSlider({
-		// auto: true,
-		pager: true
-	});
+		$.each( navLi, function(){
+			var $this = $(this),
+				link = $this.children('a');
 
-	$(".fancybox").fancybox();
-});
+			link.removeClass('active');
+
+			if ( link.attr('href') === hash ) {
+				link.addClass('active');
+			}
+		});
+	}
+
+
+	return {
+		init: function () {
+			setUpListeners();
+			$(".fancybox").fancybox();
+			$('.bxslider').bxSlider({
+				pager: true,
+				auto: true
+			});
+			$('.tracked').waypoint({
+				handler: _waypoint
+			});
+
+		}
+	};
+
+}());
+app.init();
+
